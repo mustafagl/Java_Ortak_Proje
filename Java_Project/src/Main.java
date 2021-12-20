7278d4d28b33a5e8a6f8100a32398cc1492b6efa
Gokay <Gokay@DESKTOP-LIJLR79>
2021-12-20 20:09:40 +0300

    a

--- Java_Project/src/Main.java
+++ Java_Project/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        MyFrame a =new MyFrame();
+        //MyFrame a =new MyFrame();
         Text text =new Text();
 
     }
--- Java_Project/src/Text.java
+++ Java_Project/src/Text.java
@@ -1,4 +1,4 @@
-	import javax.swing.*;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,16 +8,27 @@ public class Text extends JFrame {
     int total =0;
     public Text(){
         JFrame frame= new JFrame();
-        frame.setResizable(false);
+        frame.setTitle("Restoran Oyunu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(1264,841); // Ekranın boyutu
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
         JTextField text = new JTextField(10);
+
         frame.getContentPane().setLayout(new FlowLayout());
 
         JLabel label1= new JLabel();
-        label1.setForeground(Color.cyan);
-
+        label1.setForeground(Color.blue); //Siparis yazısının rengi
         label1.setText("Sipariş: ");
         frame.getContentPane().add(label1);
-        frame.getContentPane().setBackground(Color.black);
+
+        ImageIcon background = new ImageIcon("src/abackground_new.jpg"); //Arka plan fotoğrafı
+        label1.setIcon(background);
+
+        ImageIcon icon =new ImageIcon("src/dark.PNG"); //İkon
+        frame.setIconImage(icon.getImage());
 
         JButton button =new JButton("Gönder");
 
@@ -57,14 +68,11 @@ public class Text extends JFrame {
             }
         });
 
-        frame.setSize(544,110);
-        frame.setLocation(660,830);
-        
-        frame.setVisible(true);
 
-
-
-
+        text.setFont(new Font("Consolas",Font.ITALIC,20)); //Yazı fontu ve boyutu
+        text.setForeground(Color.red); //Yazının rengi
+        text.setBackground(Color.black);
+        text.setCaretColor(Color.green); //Çizgi rengi
 
     }
 }
